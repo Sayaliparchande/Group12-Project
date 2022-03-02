6f446ea1b1ff82012e08cfbd712b22fcbecaf566
Sayaliparchande <95363550+Sayaliparchande@users.noreply.github.com>
2022-03-02 15:30:20 +0530

    udated code

---
+++ Finalproject/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finalproject.Controllers
+{
+    public class AdminController : Controller
+    {
+        BurgerDbContext dbContext = new BurgerDbContext();
+
+        [Route("api/admin")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Admin>>> GetProducts()
+        {
+            return await dbContext.Admins.ToListAsync();
+        }
+    }
+}
---
+++ Finalproject/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finalproject.Controllers
+{
+    /*[Route("api/[controller]")]
+    [ApiController]*/
+    public class UserController : Controller
+    {
+        BurgerDbContext dbContext = new BurgerDbContext();
+
+         [Route("api/user")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SignupUser>>> GetProducts()
+        {
+            return await dbContext.SignupUsers.ToListAsync();
+        }
+
+         [Route("api/adduser")]
+        [HttpPost]
+        public IActionResult Create(SignupUser newuser)
+        {
+            dbContext.SignupUsers.Add(newuser);
+            dbContext.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
